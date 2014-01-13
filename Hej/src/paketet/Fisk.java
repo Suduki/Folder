6aac2c53c7342853a3534dbb32e23f58792c0549
Suduki <olle.lenti@gmail.com>
2014-01-13 22:34:02 +0100

    Added disp.create

--- Hej/src/paketet/Fisk.java
+++ Hej/src/paketet/Fisk.java
@@ -1,10 +1,17 @@
 package paketet;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
 public class Fisk {
 
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws LWJGLException {
 		System.out.println("111");
 		System.out.println("222");
-		System.out.println("333");
+		Display.create();
 	}
+	
+	
+	
 }
