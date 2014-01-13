e9e023ca571777f6053f476077efd8c828f05ebf
Akkiee <markus.pogulis@gmail.com>
2014-01-13 22:35:12 +0100

    Tried some stuff with LWJGL

---
+++ Hej/src/paketet/Main.java
@@ -0,0 +1,27 @@
+package paketet;
+
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("Hello, LWJGL!");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()) {
+			// Render
+			
+			Display.update();
+			Display.sync(60);
+		}
+		Display.destroy();
+	}
+}
