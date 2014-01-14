6ed5b1d8a1ca7f9edd700c8d712617ae752482c0
Suduki <olle.lenti@gmail.com>
2014-01-14 21:02:11 +0100

    hej

--- Hej/src/paketet/Vector.java
+++ Hej/src/paketet/Vector.java
@@ -1,26 +1,26 @@
 package paketet;
 
 public class Vector {
-    private int x, y;
+    private double x, y;
     
-    public Vector(int x, int y) {
+    public Vector(double x, double y) {
         this.setX(x);
         this.setY(y);
     }
 
-    public int getX() {
+    public double getX() {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setX(double d) {
+        this.x = d;
     }
 
-    public int getY() {
+    public double getY() {
         return y;
     }
 
-    public void setY(int y) {
+    public void setY(double y) {
         this.y = y;
     }
 }
